f5266286b1ad2d9793f35dea2624909cd7370bf0
almax87 <amax87@mail.ru>
2025-02-09 12:05:05 +0300

    reworked

--- Lesson2/Task2/Script-2.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE if not exists employee(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    department VARCHAR(100) NOT NULL,
-    boss_id INT,
-    FOREIGN KEY (boss_id) REFERENCES employee(id)
-);
